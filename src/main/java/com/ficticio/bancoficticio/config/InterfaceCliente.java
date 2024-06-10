a4ea38b7a80073b52f4caece759fc8c4f68a500b
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-06-10 16:39:53 -0300

    :sparkles: feat: add interface cliente
    Interface com atributos básicos da entidade cliente

---
+++ src/main/java/com/ficticio/bancoficticio/config/InterfaceCliente.java
@@ -0,0 +1,18 @@
+package com.ficticio.bancoficticio.config;
+
+public interface InterfaceCliente {
+    String getCpf();
+    void setCpf(String cpf);
+    String getNome();
+    void setNome(String nome);
+    String getEmail();
+    void setEmail(String email);
+    String getTelefone();
+    void setTelefone(String telefone);
+    String getEndereco();
+    void setEndereco(String endereco);
+    double getRendaMensal();
+    void setRendaMensal(double rendaMensal);
+    String getSenha();
+    void setSenha(String senha);
+}
