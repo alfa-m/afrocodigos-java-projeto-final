e267c83562b5af8cb9ab8791f24c66ee651b14c4
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-06-10 15:40:39 -0300

    :tada: init: adiciona os diretórios base do projeto
    Adiciona diretórios gerados pelo Spring Initializr para a configuração de uma aplicação Spring Boot

---
+++ src/main/java/com/ficticio/bancoficticio/BancoficticioApplication.java
@@ -0,0 +1,13 @@
+package com.ficticio.bancoficticio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoficticioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoficticioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ficticio/bancoficticio/BancoficticioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ficticio.bancoficticio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoficticioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
