95614b2297af891c7b5a5b7c1d81b2a0b8ba6572
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-06-12 11:17:39 -0300

    :recycle: refactor: fix tipo de arquivo (class -> interface) e add imports

--- src/main/java/com/ficticio/bancoficticio/repository/ClienteRepository.java
+++ src/main/java/com/ficticio/bancoficticio/repository/ClienteRepository.java
@@ -1,4 +1,10 @@
 package com.ficticio.bancoficticio.repository;
 
-public class ClienteRepository {
+import com.ficticio.bancoficticio.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+
 }
