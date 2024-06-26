fa788d8d277309f442bc5cd2e921aa81d27ceefe
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-06-26 09:25:33 -0300

    :sparkles: feat: add rota de home

--- src/main/java/com/ficticio/bancoficticio/BancoFicticioApplication.java
+++ src/main/java/com/ficticio/bancoficticio/BancoFicticioApplication.java
@@ -2,9 +2,17 @@ package com.ficticio.bancoficticio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BancoFicticioApplication {
+	@GetMapping("")
+	public String home(){
+		return "API do Banco Fictício";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BancoFicticioApplication.class, args);
 	}
